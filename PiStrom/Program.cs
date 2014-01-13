bda8fad51a6d72ea35c5c2c35d27658c699080eb
Arne Kiesewetter <kiesewetter.arne@t-online.de>
2014-01-14 00:15:36 +1300

    Borky test thing as proof-of-concept

---
+++ PiStrom/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PiStrom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Waiting for Connections");
+            FileServer fS = new FileServer();
+            fS.Start();
+            Console.ReadLine();
+        }
+    }
+}
